3cbe61fa0c25560d1da0d949238c6603b18ec5d6
Anthony-de-Hoog <37657@ma-web.nl>
2024-12-12 09:32:02 +0100

    Update MouseClick.cs

--- DarkestDungeonVS/Assets/Scripts/Mouse-Click/MouseClick.cs
+++ DarkestDungeonVS/Assets/Scripts/Mouse-Click/MouseClick.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class MouseClick : MonoBehaviour
 {
     [SerializeField] private DamageSystem damageSystem;
-    private enum State { Deactive, Active }
+    private enum State { Deactive, Active } // Makes it possible to give an active state or a deactive state
     private State myState = State.Deactive;
 
     public System.Action OnMouseClickUsed; // Event to notify when the script is used
@@ -12,7 +12,7 @@ public class MouseClick : MonoBehaviour
     {
         if (damageSystem == null)
         {
-            damageSystem = GetComponent<DamageSystem>();
+            damageSystem = GetComponent<DamageSystem>(); // Is misschien optioneel, maar ik behoud het even tot we zeker weten dat we het niet nodig hebben
         }
     }
 
@@ -20,11 +20,11 @@ public class MouseClick : MonoBehaviour
     {
         if (myState == State.Deactive)
         {
-            GetComponent<Renderer>().material.color = Color.white;
+            GetComponent<Renderer>().material.color = Color.white; // Deactive state that keeps the object color white
         }
         else if (myState == State.Active)
         {
-            GetComponent<Renderer>().material.color = Color.red;
+            GetComponent<Renderer>().material.color = Color.red; // Active state that makes the object red
 
             if (Input.GetMouseButtonDown(0))
             {
@@ -33,7 +33,6 @@ public class MouseClick : MonoBehaviour
                     damageSystem.Damage();
                     this.enabled = false; // Deactivate the script after use
                     OnMouseClickUsed?.Invoke(); // Notify that the script was used
-                    Debug.Log("MouseClick script deactivated after use.");
                 }
             }
         }
@@ -41,11 +40,11 @@ public class MouseClick : MonoBehaviour
 
     private void OnMouseOver()
     {
-        myState = State.Active;
+        myState = State.Active; // Activates the object when the mouse hovers over the object
     }
 
     private void OnMouseExit()
     {
-        myState = State.Deactive;
+        myState = State.Deactive; // Deactivates the object when the mouse when the mouse hovers away from the object
     }
 }
